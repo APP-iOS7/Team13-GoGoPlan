def89c2eabb6f2a26e9f09acd8770c2b882fe724
LSH89-Ligardo <cdplay89@gmail.com>
2025-02-06 11:32:16 +0900

    에러

---
+++ UIImageView.swift
@@ -0,0 +1,18 @@
+//
+//  UIImageView.swift
+//  GoGoPlan
+//
+//  Created by Seonghoon Lee on 2/6/25.
+//
+
+import SwiftUI
+
+struct UIImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIImageView()
+}
