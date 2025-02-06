d8ff38d4fe3616ea0ab297e24053c43553975fe3
LSH89 <cdplay89@gmail.com>
2025-02-07 04:59:52 +0900

    Delete GoGoPlan directory

--- GoGoPlan/Services/LocationService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol LocationServiceProtocol {
-    // 장소의 위치(위도, 경도) 값을 리턴
-    func getCurrentLocation() async throws -> (latitude: Double, longitude: Double)
-    // 위치 권한 요청 함수
-    func requestLocationPermission() async throws
-}
